cf389a946763c854643326fb704e7d03dcc82474
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-01-25 17:17:09 +0530

    Create 03_change_permission_ownership_of_files_folders.sh

---
+++ 03_change_permission_ownership_of_files_folders.sh
@@ -0,0 +1,9 @@
+
+#change ownership of a folder recursively
+sudo chown -R username:groupname folder_name
+
+#give all permission to a file
+chmod +x filename
+
+#read write all prmission : folder green
+chmod -R 777 folder_name
