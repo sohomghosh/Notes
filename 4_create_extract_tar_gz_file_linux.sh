02592dbecf7842b933c8e993203c396b8e1c09fc
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-03-30 16:39:40 +0530

    Update 4_create_extract_tar_gz_file_linux.sh

--- 4_create_extract_tar_gz_file_linux.sh
+++ 4_create_extract_tar_gz_file_linux.sh
@@ -1,7 +1,8 @@
-tar xvzf file.tar.gz
-
-#create a tar.gz fie
+#create a tar.gz fie from a folder
 tar -zcvf tar-archive-name.tar.gz source-folder-name 
+
+#create a tar.gz fie from a file fname.csv
+tar -zcvf fname.tar.gz fname.csv
 
 #extract a tar.gz file
 tar -zxvf tar-archive-name.tar.gz 
