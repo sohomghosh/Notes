918686c6e43dc54567c64ec8306c5bc1ee5ceffb
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-04-12 16:38:42 +0530

    Update see_memory-ram-Usage_numberOfCores_hardDisk.sh

--- see_memory-ram-Usage_numberOfCores_hardDisk.sh
+++ see_memory-ram-Usage_numberOfCores_hardDisk.sh
@@ -10,3 +10,8 @@ df -lk
 
 #Disk usage
 du
+
+#HDD to SSD verify for linux
+cat /sys/block/sda/queue/rotational
+#You should get 1 for hard disks and 0 for a SSD. Reference: https://unix.stackexchange.com/questions/65595/how-to-know-if-a-disk-is-an-ssd-or-an-hdd
+
