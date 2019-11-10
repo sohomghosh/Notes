d93816befd86361edcb165cbb3a55044c9fc63d7
Sohom Ghosh <sohom1ghosh@gmail.com>
2019-11-10 17:16:31 +0530

    Create select_random_lies_from_a_file.sh

---
+++ select_random_lies_from_a_file.sh
@@ -0,0 +1,4 @@
+#Select random samples / lines from a file:    
+shuf -n N input_file.txt > output_file.txt  
+#[ N is the umber of n is the number of lines to be selected randomly]
+#Reference: https://unix.stackexchange.com/questions/108581/how-to-randomly-sample-a-subset-of-a-file
