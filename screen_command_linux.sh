f18b8b17c21d9147e5e5cc98f9e91233ca3a67af
Sohom Ghosh <sohom1ghosh@gmail.com>
2020-03-23 09:44:09 +0530

    Create screen_command_linux.sh

---
+++ screen_command_linux.sh
@@ -0,0 +1,20 @@
+# Used to keep sessions running in the background
+
+$screen --version
+
+#To install sudo apt install screen OR sudo yum install screen
+
+$screen -S <name_of_the_screen>
+
+#Do your staffs here
+
+#To exit
+$CTRL + A + D
+
+#To see list of all screens running
+$screen -list
+
+#To go back to a screen
+screen -r <name_of_the_screen> 
+
+#Reference: https://linuxize.com/post/how-to-use-linux-screen/
