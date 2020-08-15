1d50488e7cf50b1fa7581f3c5449519c4a72eebc
Sohom Ghosh <sohom1ghosh@gmail.com>
2020-08-15 19:33:15 +0530

    Create watch_tail_of_a_file_continuously.sh

---
+++ watch_tail_of_a_file_continuously.sh
@@ -0,0 +1,7 @@
+#To watch a file for changes with the tail command pass the -f option
+#This will show the last ten lines of a file and will update when new lines are added
+#This is commonly used to watch log files in real-time
+
+$tail -F <file_name>
+
+#Source: https://shapeshed.com/unix-tail/
